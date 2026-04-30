ff8fb32be7620374dcc40bc5cceb69fc86bfacd7
rroossyyiidd <rroossyyiidd@gmail.com>
2026-04-30 14:19:19 +0700

    first commit

---
+++ Todo.cs
@@ -0,0 +1,7 @@
+// A model is a class that represents data that the app manages.
+public class Todo
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public bool IsComplete { get; set; }
+}
